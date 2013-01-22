67d68c5d1d24ab684b75c110fec9c7a500bcf5ae
KAWAKITA Hirofumi <hrfm0818@gmail.com>
2013-01-23 01:53:12 +0900

    AIR can't use broadcast address. but OSCSyncManager need using broadcast address. When you use OSCSyncManager. You must be exec UDPBroadcaster.jar.

---
+++ UDPBoadcaster/src/org/sgmnt/UDPBroadcaster.java
@@ -0,0 +1,82 @@
+package org.sgmnt;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+public class UDPBroadcaster {
+	
+	public static void main( String[] args ) throws Exception{
+		
+		int fromPort = 57578;
+		int toPort = 57577;
+		
+        String broadcastAddr = "255.255.255.255";
+        
+		DatagramSocket sock;
+		DatagramPacket packet;
+        InetSocketAddress remoteAddr;
+        byte receiveBuffer[];
+        
+		for ( int i = 0; i < args.length; i++ ) {
+			if ("-from".equals(args[i])) {
+				fromPort = Integer.parseInt(args[++i]);
+			} else if ("-to".equals(args[i])) {
+				toPort = Integer.parseInt(args[++i]);
+			} else if( "-broadcast".equals(args[i]) ){
+				broadcastAddr = args[++i];
+			}
+		}
+		
+        // --- Check Broadcast Address. ---
+        
+        if( broadcastAddr.equals("255.255.255.255") ){
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+    		while (interfaces.hasMoreElements()) {
+        		NetworkInterface networkInterface = interfaces.nextElement();
+        		if ( networkInterface.isLoopback() ){
+        			continue; // Don't want to broadcast to the loopback interface
+        		}
+        		for (InterfaceAddress interfaceAddress : networkInterface.getInterfaceAddresses()) {
+        			InetAddress broadcast = interfaceAddress.getBroadcast();
+        			if (broadcast == null) continue;
+        			broadcastAddr = broadcast.getHostAddress();
+        		}
+    		}
+        }
+		
+        // --- Create Socket. ---
+		
+		sock = new DatagramSocket( fromPort );
+		
+        // 受け付けるデータバッファとUDPパケットを作成
+        receiveBuffer = new byte[1024];
+        packet = new DatagramPacket( receiveBuffer, receiveBuffer.length );
+        
+        remoteAddr = new InetSocketAddress( broadcastAddr, toPort );
+
+		System.out.println( "Broadcast " + broadcastAddr + " from " + fromPort + " to " + toPort );
+		
+        while (true) {
+        	
+            sock.receive(packet);
+            
+            /*
+            // 受信したデータを標準出力へ出力
+            System.out.println(
+            	new String( packet.getData(), 0, packet.getLength() )
+            );
+            */
+            
+            byte[] buf = packet.getData();
+            sock.send( new DatagramPacket(buf, buf.length, remoteAddr ) );
+            
+        }
+        
+	}
+	
+}
